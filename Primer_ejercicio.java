a2c45059e831affaf40ee2351c8632011c436fc3
HenryGalvezQ <hgalvezq@unsa.edu.pe>
2022-04-28 19:18:57 -0500

    Repositorio individual terminado

--- Primer_ejercicio.java
+++ Primer_ejercicio.java
@@ -1,5 +1,6 @@
 public class Primer_ejercicio {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo soy Henry G :3!");
+        System.out.println("Repositorio individual terminado");
     }
 }
